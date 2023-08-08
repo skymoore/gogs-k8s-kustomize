b2231871de00423366095ac1db30ae0ff536e0ec
Sky Moore <i@msky.me>
2023-08-08 19:38:29 +0400

    gitea sucks

--- environments/git.rwx.dev/config/vault/vault-config.sh
+++ environments/git.rwx.dev/config/vault/vault-config.sh
@@ -3,18 +3,18 @@
 export VAULT_ADDR="https://vlt.rwx.dev"
 export VAULT_TOKEN=""
 
-vault policy write gogs - <<EOF
-path "secrets/data/gogs" {
+vault policy write gitea - <<EOF
+path "secrets/data/gitea" {
   capabilities = ["read", "list"]
 }
 EOF
 
-vault write auth/k8s-skydive/role/gogs \
-    bound_service_account_names=gogs \
-    bound_service_account_namespaces=gogs \
-    policies=gogs \
+vault write auth/k8s-skydive/role/gitea \
+    bound_service_account_names=gitea \
+    bound_service_account_namespaces=gitea \
+    policies=gitea \
     ttl=24h
 
 # vault secrets enable -version=2 -path=secrets kv
 
-vault kv put secrets/gogs @secret.json
+vault kv put secrets/gitea @secret.json
